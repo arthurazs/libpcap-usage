25db5f697be31a2566eeb4126930049ae0e0f489
Arthur Zopellaro <github@zope.me>
2025-01-15 22:21:38 -0300

    wip, feat: list all devs

--- src/dev.c
+++ src/dev.c
@@ -1,16 +1,36 @@
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wpadded"
 #include <pcap.h>
-#include <stdlib.h>
+#pragma clang diagnostic pop
 
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#include <stdlib.h>
+#include <netinet/in.h>
+
 
 int main(void) {
-  char *dev, errbuf[PCAP_ERRBUF_SIZE];
+  char errbuf[PCAP_ERRBUF_SIZE];
+  pcap_if_t * alldevs;
+  // TODO: get lo dev and try pcap_sendpacket
+  /*pcap_if_t dev;*/
 
-  dev = pcap_lookupdev(errbuf);
-  if (dev == NULL) {
-    fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+  int rc = pcap_findalldevs(&alldevs, errbuf);
+  if (rc == PCAP_ERROR) {
+    fprintf(stderr, "pcap_findalldevs: %s\n", errbuf);
     return EXIT_FAILURE;
   }
-  printf("Default device: %s\n", dev);
+
+  printf("Listing devices...\n");
+  while (alldevs != NULL) {
+    printf("- %s", alldevs->name);
+    if (alldevs->description != NULL) {
+      printf(": %s", alldevs->description);
+    }
+    printf("\n");
+    alldevs = alldevs->next;
+  }
+  printf("Done!\n");
+
+  pcap_freealldevs(alldevs);
+  alldevs = NULL;
   return EXIT_SUCCESS;
 }
